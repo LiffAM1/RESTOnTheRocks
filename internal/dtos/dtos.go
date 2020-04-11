237c312c4dca82e42e357e4051b68284139adccc
Abigail <abigailliff@gmail.com>
2020-04-10 20:53:45 -0400

    updated DTO

--- internal/dtos/dtos.go
+++ internal/dtos/dtos.go
@@ -3,26 +3,20 @@ package dtos
 type Recipe struct {
 	Id int
 	Name, Url string
-	//RecipeIngredients []*RecipeIngredient
+	RecipeIngredients []RecipeIngredient
 }
 
 // RecipeIngredient represents an ingredient that's used in a recipe
 type RecipeIngredient struct {
 	Id int
-	Ingredient *Ingredient
+	Ingredient Ingredient
 	Amount float32
 	Unit string // We convert all measurement units to ounces
-}
-
-// IngredientMembership maintains the mapping of ingredients in each recipe
-type IngredientMembership struct {
-	IngredientId int
-	RecipeId int
 }
 
 type Ingredient struct {
 	Id int
 	Name string
-	Recipes []string // List of Recipe Id's
 }
+
 
